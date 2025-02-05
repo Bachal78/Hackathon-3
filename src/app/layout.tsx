e9c5c24b99af5e39ecec758599b9a676d361d41b
Bachal78 <160064284+Bachal78@users.noreply.github.com>
2025-02-06 00:42:45 +0500

    My Hackathon Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import AnnouncementBar from "@/components/AnnouncementBar";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Providers from "./providers";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
         {children}
         <Footer/>
         </Providers>
+        <Analytics/>
         </body>
     </html>
   );
